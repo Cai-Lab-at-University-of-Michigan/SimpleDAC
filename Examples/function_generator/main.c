7fd03a45aff2d7c704ec65056ea5a570662f8971
Logan Walker <loganaw@umich.edu>
2024-05-06 12:22:24 -0400

    Adding function generator code

--- Examples/function_generator/main.c
+++ Examples/function_generator/main.c
@@ -4,5 +4,13 @@
 #include "pico/stdlib.h"
 
 void main() {
+    initialize_simpledac();
 
+    uint16_t v = 0;
+
+    while(1) {
+        set_value_simpledac(0, v);
+        set_value_simpledac(1, v);
+        v++;
+    }
 }
--- simpledac.c
+++ simpledac.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 // RPI-specific libraries
 #include "pico/stdlib.h"
