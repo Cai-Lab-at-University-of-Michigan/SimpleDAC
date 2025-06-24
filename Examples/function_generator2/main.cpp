55871650f09c2f17f6e0504dd8cc462ed4d5714e
Logan Walker <loganaw@umich.edu>
2025-06-24 13:39:26 -0400

    Revert changes to function generator

--- Examples/function_generator2/main.cpp
+++ Examples/function_generator2/main.cpp
@@ -9,14 +9,14 @@ int main() {
     initialize_simpledac();
 
     // Counter
-    uint16_t v = 32768; // 0;
+    uint16_t v = 0;
 
     // Loop forever
     while(1) {
         // Write value and increment
         set_value_simpledac(0, v);
         set_value_simpledac(1, v);
-        //v += 1024;
+        v += 1024;
 
         sleep_ms(1);
     }
