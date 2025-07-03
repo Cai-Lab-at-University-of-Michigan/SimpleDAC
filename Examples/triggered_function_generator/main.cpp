a40cda25710873869f06af4581ee6715bceaf675
Logan Walker <loganaw@umich.edu>
2025-07-03 12:10:48 -0400

    Update function generator

--- Examples/triggered_function_generator/main.cpp
+++ Examples/triggered_function_generator/main.cpp
@@ -7,38 +7,91 @@
 
 #define INPUT_PIN 28
 
-int main() {
+#define TRIGGER 0 // Trigger mode
+#define FUNCT 5   // Function to output (see below)
+
+/*
+    1 => Constant value
+*/
+
+#define PERIOD 10000 // us
+
+int main()
+{
     stdio_init_all();
 
     initialize_simpledac();
-    set_value_simpledac(0, 0);
-    set_value_simpledac(1, 0);
 
     // Initialize Input Pin
     gpio_init(INPUT_PIN);
     gpio_set_dir(INPUT_PIN, GPIO_IN);
 
     // Loop forever
-    while(1) {
+    while (1)
+    {
         // Wait for trigger
-        while(!gpio_get(INPUT_PIN));
-
-        for(size_t i = 0; i < 128 * 2 * 2; i++){
-            uint16_t v = 128 * i;
-
-            // Write value and increment
-            set_value_simpledac(0, v);
-            set_value_simpledac(1, v);
-
-            sleep_us(125);
+        if (TRIGGER)
+        {
+            while (!gpio_get(INPUT_PIN))
+                ;
         }
 
-        set_value_simpledac(0, 0);
-        set_value_simpledac(1, 0);
+        uint64_t start = time_us_64();
+        while (time_us_64() < start + PERIOD)
+        {
+            uint16_t v = 0;
+            float phase = time_us_64() - start;
+            phase /= PERIOD; // 0...1 phase
 
-        // Wait for trigger to return
-        while(gpio_get(INPUT_PIN));
+            switch (FUNCT)
+            {
+            case 1: // Constant Value
+                v = 32768;
+                break;
+            case 2: // Sawtooth
+                phase *= 65535;
+                v = phase;
+                break;
+            case 3: // Square
+                if (phase < 0.5)
+                {
+                    v = 65535;
+                }
+                else
+                {
+                    v = 0;
+                }
+                break;
+            case 4: // Sine wave
+                phase = (sin(phase * 3.14159 * 2) + 1.0) / 2.0;
+                phase *= 65535;
+                v = phase;
+                break;
+            case 5: // Triangle
+                if (phase > 0.5)
+                {
+                    phase -= 0.5;
+                    phase /= 0.5;
+                } else {
+                    phase /= 0.5;
+                    phase = 1.0 - phase;
+                }
+                phase *= 65535;
+                v = phase;
+                break;
+            }
+
+            set_value_simpledac(0, v);
+            set_value_simpledac(1, v);
+        }
+
+        // Wait for trigger to return to neutral
+        if (TRIGGER)
+        {
+            while (gpio_get(INPUT_PIN))
+                ;
+        }
     }
-    
+
     return 0;
 }
